0929ea340d07c96cb3ed72349e1563aab83239f7
vatsalborda1 <vborda@usf.edu>
2023-04-17 11:12:19 -0400

    QUERYSELECTORFIX

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 //import logo from './logo.svg';
 import './App.css';
+import Aboutus from './Components/Aboutus';
 import Navbar from './Components/Navbar';
-import TextForm from './Components/TextForm';
+//import TextForm from './Components/TextForm';
 
 function App() {
   return (
     <>
     <Navbar defabt='Check' appname='TextUtil'/>
-    <div className='container my-3 row='>
+    <Aboutus/>
+    
+    {/* <div className='container my-3 row='>
     <TextForm heading="Enter your text below"/>
-    </div>
+    </div> */}
     </>
   );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar(props) {
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
     <div className="container-fluid">
       <a className="navbar-brand" href="/">{props.appname}</a>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
